5a3d2a26406e68b9be848bd743cbaf16797d904e
Tom <tom@aol.com>
2021-02-25 10:22:54 +0100

    Customize action for draft and published in blog

--- config/routes.rb
+++ config/routes.rb
@@ -8,8 +8,12 @@ Rails.application.routes.draw do
   get 'contact' => 'pages#contact'
   
 
-  resources :blogs
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  resources :blogs do
+    member do
+      get :toggle_status
+    end
+  end
+  
 
   root 'pages#home'
 end
