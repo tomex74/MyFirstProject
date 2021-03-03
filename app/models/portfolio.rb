3460f86c7537927cb982dceebce6794c9a2428b6
Tom <tom@aol.com>
2021-03-03 08:34:14 +0100

    Implemented form configuration for nested attributes

--- app/models/portfolio.rb
+++ app/models/portfolio.rb
@@ -1,7 +1,7 @@
 class Portfolio < ApplicationRecord
   has_many :technologies
   accepts_nested_attributes_for :technologies, 
-                                reject_if: lambda{ |attrs| attrs['name'].blank?}
+                                 reject_if: lambda{ |attrs| attrs['name'].blank?}
 
 
   include Placeholder
